0dd462de6c93740d38301e5faa0b8136784bd09e
Rohan Pradhan <rohanprad09@gmail.com>
2020-10-09 14:43:05 +0530

    Updates

---
+++ precache-manifest.1afe3d67f34d1d63da0fcb8b8d22f70e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769d4fd1ff8366260f596efe939cc1c5",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "2e0e71aa243238b535ea",
+    "url": "/memory-game/static/css/main.522cb7ea.chunk.css"
+  },
+  {
+    "revision": "09c1bfbfb6fe4fa64c5b",
+    "url": "/memory-game/static/js/2.4121aeb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.4121aeb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0e71aa243238b535ea",
+    "url": "/memory-game/static/js/main.5b0ac92c.chunk.js"
+  },
+  {
+    "revision": "edbde91f6abb6e69834c",
+    "url": "/memory-game/static/js/runtime-main.a1f811c6.js"
+  }
+]);
